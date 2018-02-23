a02ade5d926d08d1d7c4362ddc9856cb32f1d2a5
jiikko <n905i.1214@gmail.com>
2018-02-23 22:22:17 +0900

    init

---
+++ config/puma.rb
@@ -0,0 +1,17 @@
+workers Integer(ENV['PUMA_WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 2)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  puts "hi on_worker_boot"
+end
+
+on_worker_shutdown do
+  puts "hi on_worker_shutdown"
+end
